853348131d9b8c31375f7364b4cc9f7e2d1cd169
Muhammad Fadillah Arsa <arsabandung@gmail.com>
2020-12-14 18:04:59 +0700

    tambah home

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->get('/', function() use ($router){
+    return $router->app->version();
+});
 
 $router->post('/register','UserController@register');
 $router->post('/login','UserController@login');
